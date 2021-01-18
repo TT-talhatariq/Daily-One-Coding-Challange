f8e149cb452b63e9d7c27993bad3149fd5716ab3
Talha <talhacoder01@gmail.com>
2021-01-18 09:38:56 +0500

    9th Task added

--- #09 Password Validation/C++.cpp
+++ #09 Password Validation/C++.cpp
@@ -3,18 +3,39 @@
 using namespace std;
 
 string CodelandUsernameValidation(string str) {
-  
+ 
+    //Length Testing Funtion 
+	 if(str.length() > 25 || str.length() < 4)
+  			return "false";
+	
+	//Must start with leter Testing
+	if((str[0] < 65 || str[0] > 122) || (str[0] > 90 && str[0] < 97))
+		return "false";
 
-  // code goes here
-  //It is for checking
-  return str; 
+	str[0] = str[str.length()-1];
+	//Last Character is not Underscore Testing
+	if(str[0] == '_')
+		return "false";
 
+	//Only Letters, Numbers, Underscore Chracater Testing
+	for(int i=0; i<str.length(); i++){
+		if (((str[i] < 65 || str[i] > 122) || (str[i] > 90 && str[i] < 97)) && !(str[i] <= 57 && str[i] >= 48) && str[i] != '_'){
+		
+					cout << str[i] << endl;
+					return "false";
+			}
+			}
+			
+
+	return "true";
 }
 
 int main(void) { 
    
   // keep this function call here
-  cout << CodelandUsernameValidation(coderbyteInternalStdinFunction(stdin));
+  string password;
+  cin >> password;
+  cout << CodelandUsernameValidation(password);
   return 0;
     
 }
